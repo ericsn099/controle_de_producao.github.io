e0b2ba6c99814fa4982c03e7673834f801e69b52
ericsn099 <ericsn099@hotmail.com>
2023-02-01 18:42:15 -0400

    commit

--- autoload.php
+++ autoload.php
@@ -8,16 +8,5 @@ spl_autoload_register(function ($nome_da_classe) {
     } else if (file_exists($classe_model)) {
         exit("ARQUIVO NÃO ENCONTRADO: ".$arquivo);
     }
-
-    /*$classe_controller = "../" . $nome_da_classe . ".php";
-    $classe_model = "../" . $nome_da_classe . ".php";
-    $classe_dao = "../" . $nome_da_classe . ".php";
-    if (file_exists($classe_controller)) {
-        include $classe_controller;
-    } else if (file_exists($classe_model)) {
-        include $classe_model;
-    } else if (file_exists($classe_dao)) {
-        include $classe_dao;
-    }
-    */
+    
 });
